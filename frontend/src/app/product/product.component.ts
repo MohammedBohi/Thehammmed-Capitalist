924f04bfda1c026c176face5a97cb518f83a0eff
MohammedBohi <116156394+MohammedBohi@users.noreply.github.com>
2025-03-15 01:46:50 +0100

    mes modifs

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -3,7 +3,6 @@ import { Product } from '../models/product.model';
 import {ProgressbarComponent} from '../progressbar/progressbar.component'
 import { Orientation } from '../progressbar/progressbar.component';
 import { TimerComponent } from '../timer/timer.component';
-import { WebserviceService } from '../webservice.service';
 
 
 @Component({
@@ -13,16 +12,10 @@ import { WebserviceService } from '../webservice.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-<<<<<<< HEAD
-  @Input() prod!: Product; 
-
-  //Le setter est appelé chaque fois que la valeur de l'input est modifiée,
-=======
   product!: Product;
   Orientation = Orientation;
-  
+ 
   //Le setter appelé quand valeur de l'input est modifiée,
->>>>>>> 05918923fb75e9f99ca67a7cc62e6587dfea3ef4
   //donc quand le composant parent (AppComponent) passe un nouveau Product au composant ProductComponent
   @Input()
   set prod(value: Product) {
@@ -30,8 +23,12 @@ export class ProductComponent {
     console.log('Produit mis à jour :', this.product);
     }
 
+
   incrementCount() {
     this.product.quantite++;
     console.log("Qtt:", this.product.quantite);
   }
 }
+
+
+
